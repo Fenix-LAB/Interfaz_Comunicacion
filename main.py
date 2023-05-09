642f2ae490b242f2e08649dc25b8600f773f2d4e
Christian-RH <fenixcr.123@gmail.com>
2023-05-09 13:44:57 -0600

    update

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from gui_design import *
 from PyQt5.QtSerialPort import QSerialPort
+from PyQt5.QtCore import QIODevice
 
 class MyApp(QtWidgets.QMainWindow, Ui_COM_S):
 # Se define le contructor con todos los atributos necesarios y asociacion de metodos
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+
+        self.serial = QSerialPort()
+
+        # Eventos
+        self.BTN_CONECTAR.clicked.connect(self.Conectar)
+        self.BTN_DESCONECTAR.clicked.connect(self.Desconectar)
+
+        self.serial.setBaudRate(9600)
+        self.serial.setPortName('COM4')
+
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
